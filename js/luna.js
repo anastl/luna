1d2f28ab9de5e3686da9bcddd57785b8449760ab
anastl <48775643+anastl@users.noreply.github.com>
2021-01-04 21:07:28 -0400

    Add files via upload

---
+++ js/luna.js
@@ -0,0 +1,4 @@
+function luv() {
+    var luna = [ "kind", "gentle", "strong", "resilient", "caring", "assertive", "hard-working", "reliable", "honest", "practical", "responsible", "loyal", "mature", "creative", "consistent", "appreciative", "capable", "quick", "sensitive", "perceptive", "patient", "thoughtful", "fit", "trustworthy", "shows initiative", "motivated", "versatile"];
+    document.getElementById("text").innerHTML = luna[Math.floor(Math.random() * luna.length)] ;
+}
